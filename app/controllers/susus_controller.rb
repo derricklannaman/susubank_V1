bcad3cffc3ec81fd9d5a0e4ec0603e76c36013ca
Derrick Lannaman <derricklannaman@gmail.com>
2013-01-24 20:59:12 -0500

    fixed all routes except susu/index =>"
    destroy

--- app/controllers/susus_controller.rb
+++ app/controllers/susus_controller.rb
@@ -2,6 +2,7 @@ class SususController < ApplicationController
 
   def index
     @susus = Susu.all
+    @banker = Banker.find(params[:banker_id])
   end
 
   def new
@@ -10,32 +11,40 @@ class SususController < ApplicationController
   end
 
   def create
-    @susu = Susu.new(params[:id])
+    # @susu = Susu.new(params[:id])
+
     @banker = Banker.find(params[:banker_id])
+    @susu = @banker.susus.new(params[:susu])
     if @susu.save
       flash[:notice] = "Susu created!"
       redirect_to banker_susu_path(@banker, @susu)
     else
-      flash[:notice] = "Susu not created"
+      flash[:error] = "Susu not created"
       render :new
     end
   end
 
   def show
     @susu = Susu.find(params[:id])
+    @banker = Banker.find(params[:banker_id])
   end
 
   def edit
     @susu = Susu.find(params[:id])
+    @banker = Banker.find(params[:banker_id])
   end
 
   def update
     @susu = Susu.find(params[:id])
+    @banker = Banker.find(params[:banker_id])
     @susu.update_attributes(params[:susu])
-      redirect_to @susu
+    flash[:notice] = "susu updated"
+      redirect_to banker_susu_path(@banker, @susu)
   end
 
   def destroy
+    @banker = Banker.find(params[:banker_id])
+
     Susu.find(params[:id]).destroy
       redirect_to @banker, notice: "destroy this susu?"
   end
