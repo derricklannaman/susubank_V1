e28be888529ef19b996a168d9ed37dffa0762573
Derrick Lannaman <derricklannaman@gmail.com>
2013-01-28 18:32:39 -0500

    add members views, and add routes

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,35 @@
 class MembersController < ApplicationController
+
+  def index
+    @members = Member.all
+  end
+
+  def new
+    @member = Member.new
+    @susu = Susu.find(params[:susu_id])
+  end
+
+  def create
+    @susu = Susu.find(params[:susu_id])
+    @member = @susu.members.new(params[:members])
+      if @member.save
+        redirect_to susu_member_path(@susu, @member), notice: "New member added."
+      else
+        flash[:notice] = "Something went wrong."
+        render 'new'
+      end
+  end
+
+  def show
+    @member = Member.find(params[:id])
+    @susu = Susu.find(params[:susu_id])
+  end
+
+  def edit
+    @member = Member.find(params[:id])
+    @susu = Susu.find(params[:susu_id])
+  end
+
+
+
 end
