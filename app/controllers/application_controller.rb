96b6f722f519a9cacf1143bab8d7bed7f465802e
Derrick Lannaman <derricklannaman@gmail.com>
2013-01-27 20:32:07 -0500

    add auth_user to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate_user
+
+
+  private
+
+  def authenticated_user
+    if session[:user_id]
+      @authenticated_user = Banker.find(session[:user_id])
+    else
+      @authenticated_user = nil
+    end
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-
-  # def get_banker
-  #   @banker = Banker.find(params[:id])
-  # end
-
 end
