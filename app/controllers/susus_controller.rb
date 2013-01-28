fcd26aeb71c92f240e4d933a134892430938752c
Derrick Lannaman <derricklannaman@gmail.com>
2013-01-28 14:44:35 -0500

    update susu controller w/auth_user

--- app/controllers/susus_controller.rb
+++ app/controllers/susus_controller.rb
@@ -16,11 +16,11 @@ class SususController < ApplicationController
   def create
     # @susu = Susu.new(params[:id])
 
-    @banker = Banker.find(params[:banker_id])
-    @susu = @banker.susus.new(params[:susu])
+    # @banker = Banker.find(params[:banker_id])
+    @susu = @authenticated_user.susus.new(params[:susu])
     if @susu.save
       flash[:notice] = "Susu created!"
-      redirect_to banker_susu_path(@banker, @susu)
+      redirect_to banker_susu_path(@authenticated_user, @susu)
     else
       flash[:error] = "Susu not created"
       render :new
@@ -29,17 +29,17 @@ class SususController < ApplicationController
 
   def show
     @susu = Susu.find(params[:id])
-    @banker = Banker.find(params[:banker_id])
+    @banker = @authenticated_user
   end
 
   def edit
     @susu = Susu.find(params[:id])
-    @banker = Banker.find(params[:banker_id])
+    @banker = @authenticated_user
   end
 
   def update
     @susu = Susu.find(params[:id])
-    @banker = Banker.find(params[:banker_id])
+    @banker = @authenticated_user
     @susu.update_attributes(params[:susu])
     flash[:notice] = "susu updated"
       redirect_to banker_susu_path(@banker, @susu)
