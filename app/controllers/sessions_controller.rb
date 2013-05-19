dc7627fe3016d27ff3cde6ae50bf38342687af7a
Derrick Lannaman <derricklannaman@gmail.com>
2013-05-19 17:04:00 -0400

    create Facebook session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,27 +3,52 @@ class SessionsController < ApplicationController
   end
 
   def create
-    banker = Banker.where(:banker_email => params[:banker_email]).first || Banker.from_omniauth(env["omniauth.auth"])
+    # banker = Banker.find_by_banker_email(params[:banker_email])
     # if banker && banker.authenticate(params[:password])
-      session[:user_id] = banker.id
-      flash[:notice] = "You are now signed in."
-      redirect_to banker_path(banker)
+    #   session[:user_id] = banker.id
+    #   flash[:notice] = "You are now signed in."
+    #   redirect_to banker_path(banker)
     # else
     #   flash[:error] = "Oops! Something went wrong. Please try again."
     #   render :new
     # end
+
+    banker = Banker.from_omniauth(env["omniauth.auth"])
+    session[:user_id]= banker.id
+    redirect_to banker_path(banker)
   end
 
-  # def admin_sign_in?
-  #   if @banker && @banker.authenticate(params[:password])
-  #     session[:user_id] = @banker.id
-  #   end
+    # def create
+    # if signed_in_with_facebook?
+    #   banker = Banker.from_omniauth(env["omniauth.auth"])
+    #   session[:user_id]= banker.id
+    #   redirect_to banker_path(banker)
+    # else
+    #   banker = Banker.find_by_banker_email(params[:banker_email])
+    #   if banker && banker.authenticate(params[:password])
+    #     session[:user_id] = banker.id
+    #     flash[:notice] = "You are now signed in."
+    #     redirect_to banker_path(banker)
+    #   else
+    #     flash[:error] = "Oops! Something went wrong. Please try again."
+    #     render :new
+    #   end
+    # end
+
+
+    # banker = Banker.from_omniauth(env["omniauth.auth"])
+    # session[:user_id]= banker.id
+    # redirect_to banker_path(banker)
   # end
 
   def destroy
     session[:user_id] = nil
     flash[:notice] = "You have been signed out."
     redirect_to root_path
+  end
+
+  def signed_in_with_facebook?
+   not @authenticated_user.oauth_token.nil?
   end
 
 end
